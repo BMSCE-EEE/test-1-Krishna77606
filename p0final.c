a77bdb37fec0481a61a315dd35637fa95d709900
Krishnamurthy M <>
2021-11-29 03:21:48 +0000

    final commit

--- p0final.c
+++ p0final.c
@@ -1,1 +1,6 @@
-
+#include<stdio.h>
+int main(void)
+{
+  printf("Krishnamurthy\n");
+  return 0;
+}
--- p10final.c
+++ p10final.c
@@ -1,1 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  int a, int b;
+  sum
+  printf("Krishnamurthy\n");
+  return 0;
+}
 
