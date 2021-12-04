c25e61bd87ecb54d1ba3cf7ce71d7cfc611dce90
Krishnamurthy M <95064095+Krishna77606@users.noreply.github.com>
2021-12-04 03:26:34 +0000

    commit

--- p11original.c
+++ p11original.c
@@ -1,1 +1,31 @@
+#include<stdio.h>
+float input()
+float  add(float  p, float  q)
+void out put(float  add)
+int main()
+{
+ float a ,float b;
+ a=input();
+ b=input();
+ sum=add(a,b);
+ output(sum);
+ return 0;
 
+}
+float input()
+{
+ int A;
+ printf("enter any 2 numbers\n");
+ scanf ("%f",&A);
+ return A;
+}
+float add(float p,floatq)
+{
+float S
+S=P+Q;
+return S;
+}
+void ouput(float add)
+{
+printf( "the sum of 2 numbers is=%d\n",add);
+}
